149b77094e1f18b05dd41d7469caa28002b48d77
achso13 <achmadsolehuddin23@gmail.com>
2023-07-01 16:26:19 +0700

    Setup ReactJS and TailwindCSS

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../css/app.css";
+import "./bootstrap";
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+    <React.StrictMode>
+        <h1>Hi, this is ReactJS with Laravel</h1>
+    </React.StrictMode>
+);
